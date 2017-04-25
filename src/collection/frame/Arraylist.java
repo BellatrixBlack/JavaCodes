c00ad59976c84b4b5b74d1241b8ea90032bfe341
User <User@DESKTOP-IQOQ25D>
2017-04-25 23:47:20 +0600

    First Commit

---
+++ src/collection/frame/Arraylist.java
@@ -0,0 +1,39 @@
+package collection.frame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arraylist {
+
+	public static void main(String[] args) {
+		
+		//Declaration 
+		// *Can't use the primitive type like: int
+		ArrayList<Integer> numbers= new ArrayList<Integer>();
+		
+		numbers.add(200);
+		numbers.add(30);
+		numbers.add(40);
+		
+		System.out.println(numbers.get(2));
+		
+		System.out.println("\n Iteration 1:");
+		
+		for(int i=0;i<numbers.size();i++){
+			
+			System.out.println(numbers.get(i));
+		}
+		
+		System.out.println("\n Iteration 2:");
+		
+		for(Integer value:numbers)
+		{
+			System.out.println(value);
+		}
+		 
+		
+		List<String> name=new ArrayList<String>();
+
+	}
+
+}
